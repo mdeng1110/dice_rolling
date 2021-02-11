ae0ffc4aaef487d9f062afe480878962dab3dafb
mdeng1110 <maydeng1110@yahoo.com>
2021-02-10 23:34:46 -0800

    Add files for dice rolls

---
+++ dice_rolls.py
@@ -0,0 +1,39 @@
+import random
+number = random.randint(1,6)
+# print(number)
+if number == 1:
+    print('----------')
+    print('|        |')
+    print('|    o   |')
+    print('|        |')
+    print('----------')
+elif number == 2:
+    print('----------')
+    print('|o       |')
+    print('|        |')
+    print('|       o|')
+    print('----------')
+elif number == 3:
+    print('----------')
+    print('|o       |')
+    print('|    o   |')
+    print('|       o|')
+    print('----------')
+elif number == 4:
+    print('----------')
+    print('|o      o|')
+    print('|        |')
+    print('|o      o|')
+    print('----------')
+elif number == 5:
+    print('----------')
+    print('|o      o|')
+    print('|    o   |')
+    print('|o      o|')
+    print('----------')
+else:
+    print('----------')
+    print('|o      o|')
+    print('|o      o|')
+    print('|o      o|')
+    print('----------')
